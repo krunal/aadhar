d0db70067f6251c2699b7ac4cf65d5f3a18273d2
Krunal <krunal.shah@hotmail.com>
2015-01-30 14:26:08 -0600

    Added current_user and authenticate methods

--- app/controllers/aadhar/application_controller.rb
+++ app/controllers/aadhar/application_controller.rb
@@ -1,4 +1,14 @@
-module Aadhar
-  class ApplicationController < ActionController::Base
+class Aadhar::ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  def authenticate
+  	render :status => 401, :json => { :success => false, :info => "Unauthorized"} if !current_user.present?
   end
+
+  def current_user
+    @current_user ||= User.where(authentication_token: params[:auth_token]).first
+  end
+
 end
