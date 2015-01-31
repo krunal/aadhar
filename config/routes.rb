9b362e9b3283327ce0086bd14b1bea7aced499d3
Krunal <krunal.shah@hotmail.com>
2015-01-30 18:09:38 -0600

    Modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Aadhar::Engine.routes.draw do
-  resources :users
-  resources :sessions
+  resources :users, :only => [:create]
+  resources :sessions, :only => [:create, :destroy]
   post 'passwords', :to => "passwords#update"
   post 'passwords/reset', :to => "passwords#reset"
   
